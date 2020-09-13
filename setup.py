a0ff80aaa65a6446621e952ebc2b067988ce7fe5
bronzelion <harshakc01@gmail.com>
2020-09-13 17:53:24 +0100

    Added setup.py and manifest file

---
+++ setup.py
@@ -0,0 +1,14 @@
+
+from setuptools import find_packages, setup
+
+setup(
+    name="musical_instrument_recognition",
+    version="0.0.1",
+    packages=find_packages("src/"),
+    package_dir={"": "src/"},
+    install_requires=[
+        "django==1.7",
+		"gunicorn==19.10.0"
+    ],
+    include_package_data=True,
+)
